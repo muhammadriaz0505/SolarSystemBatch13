f35aa28db3846ef7ac3bc56c2d087ef2970f481a
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-08-27 12:31:23 -0400

    solar system is ready to deplyoed

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        solarSystem inception = new solarSystem();
+        inception.Sun = 1;
+        inception.Moon = 4;
+        inception.Planets = 2;
+
+        System.out.println("the basic structure is ready ");
+    }
+}
